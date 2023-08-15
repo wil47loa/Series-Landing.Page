d086b02bb70387c03f96fae50cdef28c1875e8fe
unknown <wilmerloaizacarranza@gmail.com>
2023-08-15 00:48:05 -0500

    Subi el código

---
+++ js/web.js
@@ -0,0 +1,97 @@
+let cars = [
+    {
+      "name": "image12",
+      "title": "minivan",
+      "date": new Date('2017-01-03'),
+      "Description": "Esto es la mierda 2.0",
+      "Cost": 15000
+    },
+    {
+      "color": "red",
+      "type": "station wagon",
+      "registration": new Date('2018-03-03'),
+      "capacity": 5
+    }
+]
+
+
+const nav = document.querySelector('.nav');
+
+    window.addEventListener('scroll', function(){
+        nav.classList.toggle('active', window.scrollY >0)
+    })
+
+    
+/*------------------------------------------------------------------------------  */
+
+ //onclick section10= document.getElementById('.myimg').src='imagen10';
+
+section10.addEventListener('click',function(){
+    console.log("Esto es la mierda 2.0")
+})
+
+function fun(image, slider) {
+
+    console.log("xxzzxps: " + image.alt + " slider: " + slider);
+}
+
+function fun(image, slider) {
+
+    console.log("xxzzxps: " + image.alt + " slider: " + slider);
+}
+
+
+
+let todosSlider = [
+    {
+        "Slider": "4",
+        "Name": "image12",
+        "Title": "image12",
+        "Date": new Date('2017-01-03'),
+        "Description": "Esto es la mierda image12",
+        "Cost": 15000
+    }
+    ,
+    {
+        "slider": "4",
+        "name": "image11",
+        "title": "image11",
+        "date": new Date('2017-01-03'),
+        "Description": "Esto es la mierda image11",
+        "Cost": 20000
+    }
+]
+
+
+
+
+
+
+
+
+
+
+/*
+document.getElementById('myImage').src='pic_bulboff.gif
+
+
+
+document.getElementById("descripcion").innerHTML  = cars[0].
+
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px" onclick="fun()">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+<script>
+	function fun() {
+    	console.log("xxzzx");
+    }
+</script>
+*/
+
+
+
+    
+    
